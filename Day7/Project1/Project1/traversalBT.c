0202feda5e3468ee93442226b39427447d51ee05
sjy2335 <106982330+sjy2335@users.noreply.github.com>
2022-12-21 11:15:41 +0900

    day7-1

---
+++ Day7/Project1/Project1/traversalBT.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "traversalBT.h"
+
+// data is parant node
+// Allocate data dynamically, and assume that leftNode and rightNode were already assigned
+// then, return data
+treeNode* makeRootNode(element data, treeNode* leftNode, treeNode* rightNode) {
+	treeNode* new = (treeNode*)malloc(sizeof(treeNode));
+	new->data = data;
+	new->left = leftNode;
+	new->right = rightNode;
+}
+
+// Preorder traversal: -*AB/CD
+void preorder(treeNode* root) {
+	if (root != NULL) {
+		printf("%c", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+// Inorder traversal: A*B-C/D
+void inorder(treeNode* root) {
+	if (root != NULL) {
+		inorder(root->left);
+		printf("%c", root->data);
+		inorder(root->right);
+	}
+}
+
+// Postorder traversal: AB*CD/-
+void postorder(treeNode* root) {
+	if (root != NULL) {
+		postorder(root->left);
+		postorder(root->right);
+		printf("%c", root->data);
+	}
+}
